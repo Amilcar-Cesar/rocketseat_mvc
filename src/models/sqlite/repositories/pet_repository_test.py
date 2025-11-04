094bdbd7e0f3dd94e608a9e385339f79f25efd22
Amilcar-Cesar <amilcarcesar0@yahoo.com.br>
2025-11-04 16:28:40 -0300

    tests of the delete method of pet repository

--- src/models/sqlite/repositories/pet_repository_test.py
+++ src/models/sqlite/repositories/pet_repository_test.py
@@ -30,4 +30,13 @@ def test_list_pets():
 
     assert response[1].name == "cat"
 
+    
+def test_delete_pet():
+    mock_connection = MockConnection()
+    repo= PetsRepository(mock_connection)
 
+    repo.delete_pets("petName")
+
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.filter.assert_called_once_with(PetsTable.name == "petName")
+    mock_connection.session.delete.assert_called_once()
