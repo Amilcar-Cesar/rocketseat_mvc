2ce573d3184bb971f5ff489d5984e920968b5fd3
Amilcar Cesar <amilcarcesar0@yahoo.com.br>
2025-11-14 18:18:54 -0300

    fix: pet_lister_controller_test was with wrong Mock data

--- src/controllers/pet_lister_controller_test.py
+++ src/controllers/pet_lister_controller_test.py
@@ -4,8 +4,8 @@ from .pet_lister_controller import PetListerController
 class MockPetsRepository:
     def list_pets(self):
         return [
-            PetsTable(name="Fluffy", type="Cat", id=4),
-            PetsTable(name="Buddy", type="Dog", id=47),
+            PetsTable(name="Fluffy", type="cat", id=4),
+            PetsTable(name="Buddy", type="dog", id=47),
         ]
 
 def test_list_pets():
@@ -17,8 +17,8 @@ def test_list_pets():
             "type": "Pets",
             "count": 2,
             "attributes": [
-                { "name": "Fluffy", "id": 4 },
-                { "name": "Buddy", "id": 47 }
+                { "name": "Fluffy", "id": 4, "type": "cat" },
+                { "name": "Buddy", "id": 47, "type": "dog" }
             ]
         }
     }
