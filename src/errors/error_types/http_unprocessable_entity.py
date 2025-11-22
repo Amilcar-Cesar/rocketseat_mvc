1320d11199e7de8768d1a0609cad1ba98437f760
Amilcar Cesar <amilcarcesar0@yahoo.com.br>
2025-11-21 21:44:54 -0300

    text correction

--- src/errors/error_types/http_unprocessable_entity.py
+++ src/errors/error_types/http_unprocessable_entity.py
@@ -2,5 +2,5 @@ class HttpUnprocessableEntiyError(Exception):
     def __init__(self, message: str) -> None:
         super().__init__(message)
         self.staus_code = 422
-        self.name = 'UnprocessableEntiy'
+        self.name = 'UnprocessableEntity'
         self.message = message
